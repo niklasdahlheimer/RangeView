b154a5c97ca26f3a6f03085117835735deb5f017
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2023-12-06 11:02:30 +0100

    fix import

--- library/src/main/java/com/iammert/rangeview/library/RangeView.kt
+++ library/src/main/java/com/iammert/rangeview/library/RangeView.kt
@@ -1,7 +1,13 @@
 package com.iammert.rangeview.library
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
